ecbfa1b6a8db4f0d913a71e679dc3620795ba3dd
Wendell Richards <wrichards@Wendells-MBP.attlocal.net>
2024-11-23 22:01:55 -0600

    wx stuff

--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule Bookme.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger, :wx, :runtime_tools, :observer],
+      extra_applications: [:logger, :runtime_tools],
       mod: {Bookme.Runtime.Application, []}
     ]
   end
